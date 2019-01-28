a86d84d2e75f2d225daa00e82ea1bbb69658d4b8
vimm0 <vimmrana0@gmail.com>
2019-01-28 16:25:47 +0545

    fix model

--- apps/project/admin.py
+++ apps/project/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.project.models import Project
+from apps.project.models import Project, Language, Category
+
+projectFilter = ['id', 'modified']
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in Project._meta.fields if f.name != 'id']
+    list_display = [f.name for f in Project._meta.fields if f.name not in projectFilter]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Category._meta.fields if f.name != 'id']
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Language._meta.fields if f.name != 'id']
--- apps/project/models.py
+++ apps/project/models.py
@@ -4,19 +4,28 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 
 class Language(models.Model):
     name = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.name
+
 
 class Project(models.Model):
     name = models.CharField(max_length=256)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    language = models.ManyToManyField(Language)
     location_local = models.CharField(max_length=256)
     git_remote_url = models.CharField(max_length=256)
     achivements = models.CharField(max_length=256)
-    todo = models.CharField(max_length=256)
+    todo = models.CharField(max_length=256, null=True, blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
